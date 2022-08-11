9d25e0eeae6c236c5a94e545a0c061e61f523ff6
Your Name <you@example.com>
2022-08-10 22:08:13 -0600

    Added dependencies and created serializers, views, and local settings files.

---
+++ songs/songs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=255)
+    # super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
